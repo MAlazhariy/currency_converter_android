4990bd3902db0cca7fb8ef024748160aa376c5cd
MAlazhariy <mostafa.maok2000@gmail.com>
2023-10-06 11:35:26 +0300

    [add] main menu to toolbar

--- app/src/main/java/com/example/currencyConverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyConverter/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.currencyConverter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.core.widget.addTextChangedListener
 import java.lang.Exception
 
@@ -31,6 +33,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var calcButton: Button
     lateinit var amountV: EditText
     lateinit var resultValue: AutoCompleteTextView
+    lateinit var toolbarV: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,40 +43,82 @@ class MainActivity : AppCompatActivity() {
 
         populateDropDownMenu()
 
+        inflateMenu()
+
 
 
         amountV.addTextChangedListener {
             calculateResult()
         }
 
-        currencyFrom.addTextChangedListener{
+        currencyFrom.addTextChangedListener {
             calculateResult()
         }
 
-        currencyTo.addTextChangedListener{
+        currencyTo.addTextChangedListener {
             calculateResult()
         }
     }
 
+    private fun inflateMenu() {
+        toolbarV.inflateMenu(R.menu.main_menu)
+        toolbarV.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.share_action -> {
+                    // todo: handle share action intent here
+                }
+
+                R.id.contact_action -> {
+                    // todo: handle contact us intent here
+                }
+            }
+            Toast.makeText(
+                this,
+                "item clicked",
+                Toast.LENGTH_SHORT
+            ).show()
+            true
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(
+            R.menu.main_menu,
+            menu
+        )
+        return true
+    }
+
 
     private fun calculateResult() {
-        if(amountV.text.toString().isEmpty()){
-            amountV.setError(getString(R.string.amount_empty_err_msg))
+        if (amountV.text.toString().isEmpty()) {
+            amountV.error =
+                getString(R.string.amount_empty_err_msg)
             return
         }
 
 
 
-        try{
+        try {
             // Ration of to currency to dollar
-            val amount = amountV.text.toString().toDouble()
-            val fromRatio: Double = values[currencyFrom.text.toString()]?.toDouble() ?: 0.0
-            val toRatio: Double = values[currencyTo.text.toString()]?.toDouble() ?: 0.0
+            val amount =
+                amountV.text.toString().toDouble()
+            val fromRatio: Double =
+                values[currencyFrom.text.toString()]?.toDouble()
+                    ?: 0.0
+            val toRatio: Double =
+                values[currencyTo.text.toString()]?.toDouble()
+                    ?: 0.0
 
-            val result: Double = amount * toRatio / fromRatio
-            val formattedResult = String.format("%.3f", result)
+            val result: Double =
+                amount * toRatio / fromRatio
+            val formattedResult =
+                String.format("%.3f", result)
 
-            Log.d(TAG, "VALUES: amount: ${amountV.text}, from: ${currencyFrom.text}, to: ${currencyTo.text}")
+            Log.d(
+                TAG,
+                "VALUES: amount: ${amountV.text}, from: ${currencyFrom.text}, to: ${currencyTo.text}"
+            )
             Log.d(TAG, "----")
             Log.d(
                 TAG,
@@ -83,23 +128,40 @@ class MainActivity : AppCompatActivity() {
 
             // set result
             resultValue.setText(formattedResult)
-        } catch (e: Exception){
-            Toast.makeText(this, getString(R.string.error_occurred), Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Toast.makeText(
+                this,
+                getString(R.string.error_occurred),
+                Toast.LENGTH_SHORT
+            ).show()
             amountV.setText("")
         }
     }
 
     private fun initViews() {
-        currencyFrom = findViewById(R.id.from_autoC)
-        currencyTo = findViewById(R.id.to_autoCompleteTextView)
+        currencyFrom =
+            findViewById(R.id.from_autoC)
+        currencyTo =
+            findViewById(R.id.to_autoCompleteTextView)
         calcButton = findViewById(R.id.calc_bu)
         amountV = findViewById(R.id.te_amount)
-        resultValue = findViewById(R.id.result_autoCompleteTextView2)
+        resultValue =
+            findViewById(R.id.result_autoCompleteTextView2)
+        toolbarV = findViewById(R.id.toolbar)
     }
 
     private fun populateDropDownMenu() {
-        val countries = listOf(currencyEGP, currencyUSD, currencyEUR, currencyGBP)
-        val adapter = ArrayAdapter(this,R.layout.dropdown_text_item, countries)
+        val countries = listOf(
+            currencyEGP,
+            currencyUSD,
+            currencyEUR,
+            currencyGBP
+        )
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.dropdown_text_item,
+            countries
+        )
 
         currencyFrom.setAdapter(adapter)
         currencyTo.setAdapter(adapter)
